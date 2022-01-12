ec0b0572f799740f2dab5c5f14f35780c58ed611
chei7tain <fredrickokwuobi@gmail.com>
2022-01-12 22:05:28 +0100

    added models for user and books

--- server/models/authorModel.ts
+++ server/models/authorModel.ts
@@ -1,16 +1,24 @@
+export {};
 const mongoose = require("mongoose");
-const authorSchema = new mongoose.Schema(
-  {
-    author: {
-      type: String,
-      unique: true,
-      required: [true, "an Author should have a name or pseudoname"],
-    },
-    age: Number,
-    address: String,
+const authorSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    unique: true,
+    required: [true, "an Author should have a name or pseudoname"],
   },
-  { timeStamps: { createdAt: "created_at" } }
-);
+  age: Number,
+  address: String,
+  books: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const Author = mongoose.model("Author", authorSchema);
 
---
+++ server/models/userModel.ts
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required"],
+  },
+  dateOfBirth: {
+    type: Date,
+    required: [true, "Date of birth is required"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "email is required"],
+  },
+  phoneNumber: {
+    type: Number,
+    unique: true,
+    required: [true, "phone number is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "confirm password is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
